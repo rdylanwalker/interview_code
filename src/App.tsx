5b1d251b45ca57771e2e88a169714b6f0ef3f9a2
Dylan Walker <dylan_walker@yahoo.com>
2021-04-17 00:14:56 -0400

    Code interview first attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEventHandler, useState} from 'react';
 
 function App() {
+  const charOrder = ['r', 't', 'b', 'c', 'a'];
+  const [examples, setExamples] = useState([
+    ['asb', 'cab', 'cat', 'rat'], // false
+    ['rat', 'cat', 'cab', 'asb'], // true
+  ]);
+
+  const [wordMatchResults, setWordMatchResults] = useState<{[key: string]: boolean[]}>({});
+
+  const updateExample = (idx: number): ChangeEventHandler<HTMLInputElement> => (event) => {
+    setExamples(prevState => {
+     prevState[idx] = event.target.value.split(',');
+     return [...prevState];
+    });
+  }
+
+  function checkWords (letters: string[],words: string[], idx: number): void {
+    setWordMatchResults(prevState => ({...prevState, [idx]: []}));
+    let wordsToCheck = [...words];
+
+    letters.forEach((letter) => {
+      if (wordsToCheck[0].includes(letter)) {
+        setWordMatchResults(prevState => {
+          const prevIdx = prevState[idx] || [];
+          return ({...prevState, [idx]: [...prevIdx, true]});
+        });
+        wordsToCheck.shift();
+      }
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        examples.map((example, idx) => (
+          <div key={idx}>
+            <span>Example {idx +1 }: </span>
+            <input defaultValue={example.join(',')} onChange={updateExample(idx)} />
+            <button onClick={() => checkWords(charOrder, example, idx)}>Check Example</button>
+            <p>Result: {(wordMatchResults[idx]?.length === example.length).toString()}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
